1d210f1ad81c8732c7a1f513e9517369313c0325
dmdoa12 <dmdoa12@gmail.com>
2016-09-30 17:48:27 +0900

    B01 수정

--- myproject/src/com/test/a/B01.java
+++ myproject/src/com/test/a/B01.java
@@ -1,33 +1,43 @@
 package com.test.a;
 
 import java.util.*;
+
 public class B01 {
 
 	public static void main(String[] args) {
 		new B01().input();
-		
+
 	}
-	void input(){
+
+	void input() {
 		Scanner s = new Scanner(System.in);
-		System.out.print("수자를 몇개 입력하실건지 정하시오:");
-		int n = s.nextInt();
-		int[] num = new int[n];
-		int max_num=Integer.MIN_VALUE,min_num=Integer.MAX_VALUE;
-		
-		for(int i=0; i<n; i++){
-			System.out.print((i+1)+"번째 숫자:");
-			num[i] = s.nextInt();
+		try {
+			System.out.print("수자를 몇개 입력하실건지 정하시오:");
+			int n = s.nextInt();
+			int[] num = new int[n];
+			int max_num = Integer.MIN_VALUE, min_num = Integer.MAX_VALUE;
+
+			for (int i = 0; i < n; i++) {
+				System.out.print((i + 1) + "번째 숫자:");
+				num[i] = s.nextInt();
+			}
+
+			for (int i = 0; i < n; i++) {
+				max_num = Math.max(max_num, num[i]);
+				min_num = Math.min(min_num, num[i]);
+			}
+
+			print("가장 큰 수는 " + max_num + "이고, 가장 작은 수는" + min_num + "입니다.");
 		}
-		
-		for(int i=0; i<n; i++){
-			max_num=Math.max(max_num,num[i]);
-			min_num=Math.min(min_num,num[i]);
+
+		finally {
+			if (s != null)
+				s.close();
 		}
-		
-		print("가장 큰 수는 "+max_num+"이고, 가장 작은 수는"+min_num+"입니다.");
 	}
-	
-	void print(String str){
+
+	void print(String str) {
 		System.out.println(str);
 	}
+
 }
