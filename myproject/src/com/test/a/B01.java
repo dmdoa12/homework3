b63af0a1b605622d63e37f140a5f66fed248bd47
dmdoa12 <dmdoa12@gmail.com>
2016-09-30 16:14:41 +0900

    B01 완성

--- myproject/src/com/test/a/B01.java
+++ myproject/src/com/test/a/B01.java
@@ -1,11 +1,33 @@
 package com.test.a;
 
+import java.util.*;
 public class B01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello2");
-		System.out.println("hello3");
+		new B01().input();
+		
 	}
-
+	void input(){
+		Scanner s = new Scanner(System.in);
+		System.out.print("수자를 몇개 입력하실건지 정하시오:");
+		int n = s.nextInt();
+		int[] num = new int[n];
+		int max_num=Integer.MIN_VALUE,min_num=Integer.MAX_VALUE;
+		
+		for(int i=0; i<n; i++){
+			System.out.print((i+1)+"번째 숫자:");
+			num[i] = s.nextInt();
+		}
+		
+		for(int i=0; i<n; i++){
+			max_num=Math.max(max_num,num[i]);
+			min_num=Math.min(min_num,num[i]);
+		}
+		
+		print("가장 큰 수는 "+max_num+"이고, 가장 작은 수는"+min_num+"입니다.");
+	}
+	
+	void print(String str){
+		System.out.println(str);
+	}
 }
